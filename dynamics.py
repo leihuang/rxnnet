89099310fa9bde776565c6e8b13fb8b9d907cc34
Lei Huang <lh389@cornell.edu>
2017-11-05 23:37:31 -0800

    cleaned up dynamics.py

--- dynamics.py
+++ dynamics.py
@@ -1,55 +1,14 @@
 """
 """
 
+from __future__ import division
+
 import pandas as pd
 from SloppyCell import daskr
 from SloppyCell.ReactionNetworks import Dynamics
 
 
 TOL = 1e-12
-
-def integrate0(net, times, varids, p=None, tol=None, to_DF=False, use_daeint=False):
-    if tol is None:
-        tol = TOL
-    
-    if times[0] != 0:
-        times = [0] + list(times)
-        _prepend_zero = True
-    else:
-        _prepend_zero = False
-        
-    if p is not None:
-        net.update_optimizable_vars(p)
-   
-    x0 = net.x0.copy()  # integration changes x0, hence the copying
-    
-    if not hasattr(net, 'res_function'):
-        net.compile()
-        
-    if use_daeint:
-        assert varids == net.xids
-        out = daskr.daeint(res=net.res_function, t=times, y0=x0, yp0=[0]*net.xdim, 
-                           atol=[tol]*net.xdim, rtol=[tol]*net.xdim, 
-                           intermediate_output=False, rpar=net.constantVarValues)
-        xt = out[0]
-        xt[0] = net.x0  # somehow daskr.daeint messes up the first timepoint
-    else:
-        # Switch to Dynamics.integrate because of presumably easier indexing 
-        # of requested varids.
-        # Be careful of Dynamics.integrate's handling of times, esp. when 
-        # the initial time is not zero.    
-        traj = Dynamics.integrate(net, times, params=p, fill_traj=False,
-                                  rtol=[tol]*net.xdim, atol=[tol]*net.xdim)
-        xt = traj.copy_subset(varids).values
-        
-    if _prepend_zero:
-        xt = xt[1:]
-        times = times[1:]
-    
-    if to_DF:
-        return butil.DF(xt, index=times, columns=varids)
-    else:
-        return xt
 
 
 class Trajectory(pd.DataFrame):
