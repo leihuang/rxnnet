a2827aaade453be762ee3a6810cd53809bb1cf3c
Lei Huang <lh389@cornell.edu>
2017-11-10 23:10:46 -0800

    added K, P, ixids, dxids, Nr, L0 and L functions to steadystate.py

--- structure.py
+++ structure.py
@@ -1,15 +1,21 @@
 """
 """
 
+from __future__ import division
 import fractions
+import re
+import subprocess
 
 import numpy as np
+import pandas as pd
 
+import util
+reload(util)
 from util import Matrix
 
 
-def get_stoich_mat(net=None, rxnid2stoich=None, 
-                   only_dynvar=True, integerize=False):
+def get_stoichiometry_matrix(net=None, rxnid2stoich=None, 
+                             only_dynvar=True, integerize=False):
     """Return the stoichiometry matrix (N) of the given network or 
     dict rxnid2stoich. Rows correspond to species, and columns correspond to 
     reactions.
@@ -20,7 +26,9 @@ def get_stoich_mat(net=None, rxnid2stoich=None,
         only_dynvar: if True, use *dynamic* species as rows (keep out 
                         constant/buffered species);
                      if False, use species as row
-        integerize: if True, make all stoichcoefs integers
+        integerize: if True, make all stoichcoefs integers; should be used with
+                    caution as the ratelaws need to be changed accordingly to
+                    keep the dynamics the same
     """
     if net:
         if only_dynvar:
@@ -28,7 +36,7 @@ def get_stoich_mat(net=None, rxnid2stoich=None,
         else:
             rowvarids = net.spids
 
-        N = Matrix(np.zeros((len(rowvarids), net.vdim)),
+        N = Matrix(np.zeros((len(rowvarids), net.vdim), dtype='int'),
                    rowvarids, net.rxnids)
 
         for spid in rowvarids:
@@ -61,20 +69,243 @@ def get_stoich_mat(net=None, rxnid2stoich=None,
         for i in range(N.ncol):
             col = N.iloc[:,i]
             nonzeros = [num for num in butil.flatten(col) if num]
-            denoms = [fractions.Fraction(nonzero).limit_denominator().denominator 
-                      for nonzero in nonzeros]
+            denoms = [fractions.Fraction(nz).limit_denominator().denominator 
+                      for nz in nonzeros]
             denom = np.prod(list(set(denoms)))
             N.iloc[:,i] = col * denom
         
     return N
 
 
-def get_pool_mul_mat(self):
-    pass
+def get_steadystate_flux_matrix(net):
+    """
+    :param net:
+    """
+    N = net.N
+    if hasattr(net, 'ss_flux_mat'):
+        K = net.ss_flux_mat
+        if np.allclose(N * K, [0]*net.xdim):
+            return K
+    elif N.rank == N.ncol:
+        return Matrix(None, index=net.rxnids)
+    else:
+        # The following codes compute the INTEGER basis of right null space
+        # of stoichiometry matrix.
+        
+        if np.allclose(net.N, net.N.astype('int')):
+            # convert the matrix into a string recognizable by sage
+            matstr = re.sub('\s|[a-z]|\(|\)', '', np.matrix(N).__repr__())
+    
+            # write a (sage) python script ".tmp_sage.py"
+            # for more info of the sage commands: 
+            # http://www.sagemath.org/doc/faq/faq-usage.html#how-do-i
+            # -import-sage-into-a-python-script
+            # http://www.sagemath.org/doc/tutorial/tour_linalg.html
+            f = open('.tmp_sage.py', 'w')
+            f.write('from sage.all import *\n\n')
+            if np.allclose(net.N, net.N.astype('int')):
+                f.write('A = matrix(ZZ, %s)\n\n' % matstr)  # integer field
+            else:
+                f.write('A = matrix(RR, %s)\n\n' % matstr)  # real field
+            f.write('print kernel(A.transpose())')  # right nullspace vectors
+            f.close()
+            
+            # call sage and run .tmp_sage.py
+            out = subprocess.Popen(['sage', '-python', '.tmp_sage.py'],
+                                   stdout=subprocess.PIPE)
+
+            # process the output from sage
+            vecstrs = out.communicate()[0].split('\n')[2:-1]
+            #vecs = [eval(re.sub('(?<=\d)\s*(?=\d|-)', ',', vec)) 
+            #        for vec in vecstrs]
+            vecs = [filter(None, vec.strip('[]').split(' ')) for vec in vecstrs]
+            try:
+                vecs = [[int(elem) for elem in vec if elem] for vec in vecs]
+            except ValueError:
+                vecs = [[float(elem) for elem in vec if elem] for vec in vecs]
+            fdids = ['J%d'%idx for idx in range(1, len(vecs)+1)]
+            K = Matrix(np.transpose(vecs), net.rxnids, fdids)
+            
+        else:
+            raise NotImplementedError
+
+        net.ss_flux_mat = K
+
+        return K
 
 
-def get_ss_flux_mat(self):
-    pass
+def get_pool_multiplicity_matrix(net):
+    """Return a matrix whose row vectors are the multiplicities of 
+    dynamic variables in the conservation pools. Mathematically, the matrix 
+    has its rows spanning the left null space of the stoichiometry matrix.
+    
+    The function is computationally costly, because it calls *sage* to perform 
+    matrix computations over the integer ring. 
+    (Note that the matrix is converted to floats before being returned.)
+    """
+    N = net.N
+    if hasattr(net, 'pool_mul_mat'):
+        P = net.pool_mul_mat
+        if np.allclose(P * N, [0]*net.vdim):
+            return P
+    elif N.rank == N.nrow:
+        return Matrix(None, columns=net.xids)
+    else:
+        # The following codes compute the INTEGER basis of left null space
+        # of stoichiometry matrix.
 
+        # Convert the matrix into a string recognizable by sage.
+        matstr = re.sub('\s|[a-z]|\(|\)', '', np.matrix(N).__repr__())
+    
+        ## Write a (sage) python script ".tmp_sage.py".
+        # for more info of the sage commands: 
+        # http://www.sagemath.org/doc/faq/faq-usage.html#how-do-i
+        # -import-sage-into-a-python-script
+        # http://www.sagemath.org/doc/tutorial/tour_linalg.html
+        f = open('.tmp_sage.py', 'w')
+        f.write('from sage.all import *\n\n')
+        if np.allclose(net.N, net.N.astype('int')):
+            f.write('A = matrix(ZZ, %s)\n\n' % matstr)  # integer field
+        else:
+            f.write('A = matrix(RR, %s)\n\n' % matstr)  # real field
+        f.write('print A.kernel()')  # this returns the left nullspace vectors
+        f.close()
+
+        # Call sage and run .tmp_sage.py.
+        out = subprocess.Popen(['sage', '-python', '.tmp_sage.py'],
+                               stdout=subprocess.PIPE)
+        
+        # Process the output from sage.
+        vecstrs = out.communicate()[0].split('\n')[2:-1]
+        vecs = [eval(re.sub('(?<=\d)\s+(?=\d|-)', ',', vec)) for vec in vecstrs]
+
+        poolids = ['Pool%d'%idx for idx in range(1, len(vecs)+1)]
+        #poolids = net.xids[-len(vecs):][::-1]
+        P = Matrix(vecs, poolids, N.rowvarids)
+
+        # Clean things up: so far P can be, eg, 
+        #        X1  X2  X3  X4
+        # Pool1   0   0   1   1  # say, adenonine backbone
+        # Pool2   2   1   3   2  # say, phospho group
+        # We want it be the following, via Gaussian row reduction:
+        #        X1  X2  X3  X4
+        # Pool1   2   1   1   0
+        # Pool2  -2  -1   0   1
+        # so that X3 and X4 as the dependent dynvars can be easily 
+        # selected and expressed as the linear combinations of 
+        # independent dynvars
+
+        # Adapted from here:
+        # http://rosettacode.org/wiki/Reduced_row_echelon_form#Python
+        def _to_reduced_row_echelon_form(m):
+            M = m.values.tolist()
+            lead = 0
+            rowCount = len(M)
+            columnCount = len(M[0])
+            for r in range(rowCount):
+                if lead >= columnCount:
+                    return
+                i = r
+                while M[i][lead] == 0:
+                    i += 1
+                    if i == rowCount:
+                        i = r
+                        lead += 1
+                        if columnCount == lead:
+                            return
+                M[i],M[r] = M[r],M[i]
+                lv = M[r][lead]
+                M[r] = [ mrx / float(lv) for mrx in M[r]]
+                for i in range(rowCount):
+                    if i != r:
+                        lv = M[i][lead]
+                        M[i] = [ iv - lv*rv for rv,iv in zip(M[r],M[i])]
+                lead += 1
+            return Matrix(M, m.rowvarids, m.colvarids)
+        
+        P = _to_reduced_row_echelon_form(P.ix[:, ::-1]).ix[::-1, ::-1]
+
+    net.pool_mul_mat = P
+
+    return P
+
+
+def get_dependent_dynamical_variable_ids(net):
+    """Return the dependent dynamical variables of the network (the dependence
+    comes from conservation pools).
+
+    For example, let P be:
+            X1  X2  X3  X4
+    Pool1   -2  -1   0   1
+    Pool2    2   1   1   0
+    
+    It should return ['X3', 'X4']
+    """
+    P = net.P
+    # dependent dynamic variables are picked at the end of each pool so that
+    # networks that have been reordered or not will give the same variables
+    if P is None:
+        dxids = []    
+    else:
+        # pick the last xid in each pool
+        dxids = [P.iloc[i][P.iloc[i]!=0].index[-1] for i in range(P.nrow)]
+        
+        # the following scenario should not be possible if P is in rref
+        if len(dxids) != len(set(dxids)):
+            # eg, ATP can be part of both the adenylate and phosphate pools;
+            # in this case, it is easier to manually pick ddynvarids
+            raise StandardError("The same dynamic variable has been chosen \
+                from multiple pools as a dependent dynamic variable")
+    return dxids
+
+
+def get_independent_dynamical_variable_ids(net):
+    """Return the independent dynamical variables of the network.
+    """
+    dxids = net.dxids
+    return [xid for xid in net.xids if xid not in dxids]
+
+
+def get_reduced_stoichiometry_matrix(net):
+    """
+    """
+    ixids = net.ixids
+    assert ixids == net.xids[:len(ixids)], "dynamical variables are not ordered"
+
+    Nr = net.N.iloc[:len(ixids)]
+    return Nr
+
+
+def get_reduced_link_matrix(net):
+    """
+    L0: L = [I ]
+            [L0]
+    """
+    ixids = net.ixids
+    assert ixids == net.xids[:len(ixids)], "dynamical variables are not ordered"
+    dxids = net.xids[len(ixids):]
+
+    if len(dxids) == 0:
+        L0 = Matrix(columns=net.xids)
+    else:
+        L0 = Matrix(-net.P.loc[:, ixids].values, dxids, ixids)
+    return L0
+
+
+def get_link_matrix(net):
+    """
+    L0: L = [I ]
+            [L0]
+            
+    N = L * Nr
+    """
+    L0 = get_reduced_link_matrix(net)
+    I = Matrix.eye(net.xids[:L0.ncol])
+    L = pd.concat((I, L0))
+    return L
+
+
+def get_modules(net):
+    raise NotImplementedError
 
 
--- tests/test_make_net.py
+++ tests/test_make_net.py
@@ -27,47 +27,95 @@ reload(network)
 
 
 @pytest.fixture()
-def net():
-    n = network.Network(id='net')
-    n.add_compartment(id='env')
-    n.add_compartment(id='cell')
-    n.add_species(id='C1', compartment='env', initial_value=2, 
-                  is_constant=True)
-    n.add_species(id='C2', compartment='env', initial_value=1, 
-                  is_constant=True)
-    n.add_species(id='X', compartment='cell', initial_value=0)
-    n.add_reaction(id='R1', eqn='C1<->X', ratelaw='k1*(C1-X)', p={'k1':1})
-    n.add_reaction(id='R2', eqn='X<->C2', ratelaw='k2*(X-C2)', p={'k2':2})
-    n.compile()
-    return n
+def nets():
+    net1 = network.Network(id='net1')
+    net1.add_compartment(id='env')
+    net1.add_compartment(id='cell')
+    net1.add_species(id='C1', compartment='env', initial_value=2, 
+                     is_constant=True)
+    net1.add_species(id='C2', compartment='env', initial_value=1, 
+                     is_constant=True)
+    net1.add_species(id='X', compartment='cell', initial_value=0)
+    net1.add_reaction(id='R1', eqn='C1<->X', ratelaw='k1*(C1-X)', p={'k1':1})
+    net1.add_reaction(id='R2', eqn='X<->C2', ratelaw='k2*(X-C2)', p={'k2':2})
+    net1.compile()
+
+    net2 = network.Network(id='net2')
+    net2.add_compartment(id='env')
+    net2.add_compartment(id='cell') 
+    net2.add_species(id='C1', compartment='env', initial_value=2,
+                     is_constant=True)  
+    net2.add_species(id='C2', compartment='env', initial_value=2,
+                     is_constant=True)
+    net2.add_species(id='C3', compartment='env', initial_value=1,
+                     is_constant=True)
+    net2.add_species(id='X1', compartment='cell', initial_value=0)
+    net2.add_species(id='X2', compartment='cell', initial_value=0)
+    net2.add_reaction(id='R1', eqn='C1+X1<->2 X2')
+    net2.add_reaction(id='R2', eqn='C2+X2<->X1')
+    net2.add_reaction(id='R3', eqn='X2<->C3')
+
+    net3 = network.Network(id='net3')
+    net3.add_compartment(id='env')
+    net3.add_compartment(id='cell') 
+    net3.add_species(id='C1', compartment='env', initial_value=2,
+                     is_constant=True)  
+    net3.add_species(id='C2', compartment='env', initial_value=2,
+                     is_constant=True)
+    net3.add_species(id='C3', compartment='env', initial_value=1,
+                     is_constant=True)
+    net3.add_species(id='X1', compartment='cell', initial_value=0)
+    net3.add_species(id='X2', compartment='cell', initial_value=0)
+    net3.add_species(id='X3', compartment='cell', initial_value=0)
+    net3.add_species(id='X4', compartment='cell', initial_value=0)
+    net3.add_reaction(id='R1', eqn='C1+X1<->2 X2')
+    net3.add_reaction(id='R2', eqn='X2+X3<->X1+X4')
+    net3.add_reaction(id='R3', eqn='C2+X4<->X3')
+    net3.add_reaction(id='R4', eqn='X2<->C3')
+
+    return [net1, net2, net3]
 
 
-def test_get_N(net):
-    assert net.N.values.tolist() == [[1.0, -1.0]]
+def test_get_N(nets):
+    net1, net2, net3 = nets
+    assert np.allclose(net1.N, [[1, -1]])
+    assert np.allclose(net2.N, [[-1, 1, 0], [2, -1, -1]])
 
 
-def test_get_K(net):
-    pass
+def test_get_P(nets):
+    net1, net2, net3 = nets
+    assert net1.P.shape == (0,1)
+    assert net2.P.shape == (0,2)
+    assert net3.P.shape == (1,4) and np.allclose(net3.P, [[0,0,1,1]])
 
 
-def test_get_P(net):
-    pass
+def test_get_K(nets):
+    net1, net2, net3 = nets
+    assert net1.K.shape == (2,1) and np.allclose(net1.K, 1)
+    assert net2.K.shape == (3,1) and np.allclose(net2.K, 1)
+
+
+def test_get_Nr(nets):
+    net1, net2, net3 = nets
+    assert net3.ix    
 
     
-def test_integrate(net):
-    assert all(np.isclose(net.integrate([0,1]), [[0.0], [1.2669505754952797]]))
-    assert all(np.isclose(net.integrate([2,3]),
+def test_integrate(nets):
+    net1, net2, net3 = nets
+    assert all(np.isclose(net1.integrate([0,1]), [[0.0], [1.2669505754952797]]))
+    assert all(np.isclose(net1.integrate([2,3]),
         [[1.330028330417476], [1.3331687869183433]]))
-    assert all(np.isclose(net.integrate((0,1)).iloc[:3],
+    assert all(np.isclose(net1.integrate((0,1)).iloc[:3],
         [[0.0], [9.31322574614828e-13], [1.8626451492290054e-12]]))
-    assert all(np.isclose(net.integrate((0,1)).iloc[-3:],
+    assert all(np.isclose(net1.integrate((0,1)).iloc[-3:],
         [[1.2653340809755658], [1.2661904908822275], [1.2669505754952797]]))
-    assert all(np.isclose(net.integrate((1,2)).iloc[[0,-1]],
+    assert all(np.isclose(net1.integrate((1,2)).iloc[[0,-1]],
         [[1.2669505754952797], [1.330028330418152]]))
 
 
-def test_get_s():
-    assert net.get_s().iloc[0] == 1.3333333333333333
+def test_get_s(nets):
+    net1, net2, net3 = nets
+    assert net1.get_s().iloc[0] == 1.3333333333333333
 
 
 def test_get_Ep():
