fb61a7f56aa9d752bb0f917471525a52d4eb9505
Lei Huang <lh389@cornell.edu>
2017-11-14 09:22:47 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,13 @@
 """
 
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
 
 
-
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(name='rxnnet',
       version='0.1.1',
       description=('A Python package for representing reaction networks '
                    'and simulating their behaviors'),
-      long_description=long_description,
       packages=find_packages(exclude=['tests', 'docs']),
       url='https://github.com/leihuang/rxnnet',
       license='MIT',
