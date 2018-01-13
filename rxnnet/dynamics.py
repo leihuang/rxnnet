d9f57aff5b1319fd92869257713bde91ae0b0295
Lei Huang <lh389@cornell.edu>
2018-01-13 12:32:42 -0800

    switch to SloppyCell for integration

--- rxnnet/dynamics.py
+++ rxnnet/dynamics.py
@@ -1,18 +1,23 @@
 """
 """
 
-from __future__ import division
+from __future__ import absolute_import, division, print_function
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from SloppyCell import daskr
+from SloppyCell.ReactionNetworks import Dynamics
+
+from rxnnet import util
 
 
-TOL = 1e-12
+
+ATOL = 1e-9
+RTOL = 1e-9
 
 
-class Trajectory(pd.DataFrame):
+class Trajectory(util.DF):
     """
     """
     @property
@@ -42,7 +47,7 @@ class Trajectory(pd.DataFrame):
         fig = plt.figure(figsize=figsize)
         ax = fig.add_subplot(111)
         if varids is not None:
-            traj = traj[varids]
+            traj = self[varids]
         else:
             traj = self
         ax.plot(traj.times, traj)
@@ -57,7 +62,7 @@ class Trajectory(pd.DataFrame):
 
 
 
-def integrate(net, times, p=None, x0=None, tol=None, varids=None):
+def integrate(net, times, p=None, x0=None, atol=None, rtol=None, varids=None):
     """A wrapper of SloppyCell.daskr.daeint.
 
     Two nonintuitive behaviors of daskr.daeint are fixed here:
@@ -67,9 +72,9 @@ def integrate(net, times, p=None, x0=None, tol=None, varids=None):
         from 0 to tmax-tmin.
 
     :param net:
-    :type net:
-    :param times:
-    :type times: list or tuple
+    :param times: list or tuple
+    :param atol:
+    :param rtol:
     :param varids: not implemented yet
     """
     if isinstance(times, tuple):
@@ -89,21 +94,36 @@ def integrate(net, times, p=None, x0=None, tol=None, varids=None):
     if x0 is None:
         x0 = net.x0
 
-    if tol is None:
-        tol = TOL
+    if atol is None:
+        atol = ATOL
+    if rtol is None:
+        rtol = RTOL
 
     if not hasattr(net, 'res_function'):
         net.compile()
-    
+
+    """
     out = daskr.daeint(res=net.res_function, t=_times, 
                        y0=x0.copy(), yp0=[0]*net.xdim, 
-                       atol=[tol]*net.xdim, rtol=[tol]*net.xdim, 
+                       atol=[atol]*net.xdim, rtol=[rtol]*net.xdim, 
                        intermediate_output=intermediate_output, 
-                       rpar=net.constantVarValues)
-
+                       rpar=net.constants)
     traj = out[0]
     traj[0] = x0
     times = out[1]
+    """
+
+    # Use SloppyCell.ReactionNetworks.Dynamics.integrate for now as it wraps 
+    # around SloppyCell.daskr and is somehow more stable than daskr itself
+    # but not much slower.
+    # We need to overwrite the Network attribute 'constantVarValues' 
+    # in SloppyCell, as it somehow does not update upon changing parameters.
+    net.constantVarValues = net.constants
+    out = Dynamics.integrate(net, _times, 
+                             atol=[atol]*net.xdim, rtol=[rtol]*net.xdim, 
+                             fill_traj=intermediate_output)
+    traj = out.values
+    times = out.timepoints
 
     if t0 != 0:
         idx_t0 = list(times).index(t0)
@@ -116,4 +136,95 @@ def integrate(net, times, p=None, x0=None, tol=None, varids=None):
                       columns=net.xids)
 
 
+"""
+def get_predict(net, expts, **kwargs):
 
+    def get_xt(net, times, varids, p=None, tol=None, to_DF=False, use_daeint=False):
+        if tol is None:
+            tol = TOL
+        
+        if times[0] != 0:
+            times = [0] + list(times)
+            prepend_zero = True
+        else:
+            prepend_zero = False
+            
+        if p is not None:
+            net.update_optimizable_vars(p)
+       
+        x0 = net.x0.copy()  # integration changes x0, hence the copying
+        
+        if not hasattr(net, 'res_function'):
+            net.compile()
+            
+        if use_daeint:
+            assert varids == net.xids
+            out = daskr.daeint(res=net.res_function, t=times, y0=x0, yp0=[0]*net.xdim, 
+                               atol=[tol]*net.xdim, rtol=[tol]*net.xdim, 
+                               intermediate_output=False, rpar=net.constantVarValues)
+            xt = out[0]
+            xt[0] = net.x0  # somehow daskr.daeint messes up the first timepoint
+        else:
+            # Switch to Dynamics.integrate because of presumably easier indexing 
+            # of requested varids.
+            # Be careful of Dynamics.integrate's handling of times, esp. when 
+            # the initial time is not zero.    
+            traj = Dynamics.integrate(net, times, params=p, fill_traj=False,
+                                      rtol=[tol]*net.xdim, atol=[tol]*net.xdim)
+            xt = traj.copy_subset(varids).values
+            
+        if prepend_zero:
+            xt = xt[1:]
+            times = times[1:]
+        
+        if to_DF:
+            return util.DF(xt, index=times, columns=varids)
+        else:
+            return xt
+
+
+    def get_dxtdp(net, times, varids, p=None, tol=None, to_DF=False):
+        if tol is None:
+            tol = TOL
+
+        if times[0] != 0:
+            times = [0] + list(times)
+            prepend_zero = True
+        else:
+            prepend_zero = False
+            
+        straj0 = Dynamics.integrate_sensitivity(net, times, params=p, rtol=tol)
+        straj = straj0.copy_subset(itertools.product(varids, net.pids))
+
+        if prepend_zero:
+            dat = np.vstack(np.hsplit(straj.values[1:], len(varids)))
+            times = times[1:]
+        else:
+            dat = np.vstack(np.hsplit(straj.values, len(varids)))
+            
+        if to_DF:
+            return util.DF(dat, index=itertools.product(varids, times), 
+                            columns=net.pids)
+        else:
+            return dat
+            
+
+    assert expts.conds == [()], 'condition is not just wildtype.'
+    #assert util.flatten(expts['varids']) == net.xids  # the restriction can be relaxed later
+    varids = util.flatten(expts['varids'])
+    
+    
+    def f(p):
+        net.update(t=0)
+        return get_xt(net, times util.flatten(expts['times']), p=p, varids=varids, **kwargs).T.flatten()
+        
+    
+    def Df(p):
+        net.update(t=0)
+        return get_dxtdp(net, times util.flatten(expts['times']), p=p, varids=varids, **kwargs)
+
+    pred = predict.Predict(f=f, Df=Df, p0=net.p0, pids=net.pids, 
+                           yids=expts.yids, expts=expts)
+    
+    return pred
+"""
