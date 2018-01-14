f4e6361975f886863a5138cd09e420806b93b7f4
Lei Huang <lh389@cornell.edu>
2018-01-13 20:13:36 -0800

    add dynamics.get_predict

--- rxnnet/dynamics.py
+++ rxnnet/dynamics.py
@@ -2,6 +2,7 @@
 """
 
 from __future__ import absolute_import, division, print_function
+import itertools
 
 import pandas as pd
 import numpy as np
@@ -10,6 +11,7 @@ from SloppyCell import daskr
 from SloppyCell.ReactionNetworks import Dynamics
 
 from rxnnet import util
+from infotopo import predict
 
 
 
@@ -63,9 +65,8 @@ class Trajectory(util.DF):
 
 
 def integrate(net, times, p=None, x0=None, atol=None, rtol=None, varids=None):
-    """A wrapper of SloppyCell.daskr.daeint.
-
-    Two nonintuitive behaviors of daskr.daeint are fixed here:
+    """A wrapper of SloppyCell.ReactionNetworks.Dynamics.integrate, where
+    two previously nonintuitive behaviors are fixed here:
     1) x(t0) always falsely takes the value of x(t1), where t0 and t1 are 
         the first and second time points in the return traj;
     2) when the argument times does not start from 0, the integration goes
@@ -75,12 +76,12 @@ def integrate(net, times, p=None, x0=None, atol=None, rtol=None, varids=None):
     :param times: list or tuple
     :param atol:
     :param rtol:
-    :param varids: not implemented yet
+    :param varids: 
     """
     if isinstance(times, tuple):
-        intermediate_output=True
+        intermediate_output = True
     else:
-        intermediate_output=False
+        intermediate_output = False
 
     t0 = times[0]
     if t0 == 0:
@@ -116,115 +117,107 @@ def integrate(net, times, p=None, x0=None, atol=None, rtol=None, varids=None):
     # Use SloppyCell.ReactionNetworks.Dynamics.integrate for now as it wraps 
     # around SloppyCell.daskr and is somehow more stable than daskr itself
     # but not much slower.
-    # We need to overwrite the Network attribute 'constantVarValues' 
-    # in SloppyCell, as it somehow does not update upon changing parameters.
-    net.constantVarValues = net.constants
+    # It automatically updates net.x hence no need to manually update x.
     out = Dynamics.integrate(net, _times, 
                              atol=[atol]*net.xdim, rtol=[rtol]*net.xdim, 
                              fill_traj=intermediate_output)
+
+    if varids is not None:
+        out = out.copy_subset(varids)
+
     traj = out.values
     times = out.timepoints
+    varids = out.key_column.keys()
 
     if t0 != 0:
         idx_t0 = list(times).index(t0)
         times = times[idx_t0:]
         traj = traj[idx_t0:]
 
-    net.x = traj[-1]
-
-    return Trajectory(traj, index=pd.Index(times, name='time'), 
-                      columns=net.xids)
+    return Trajectory(traj, index=pd.Index(times, name='time'), columns=varids)
 
 
-"""
+
+def integrate_sensitivity(net, times, p=None, x0=None, rtol=None, 
+                          varids=None):
+    """
+
+    :param net:
+    :param times: list or tuple
+    :param atol:
+    :param rtol:
+    :param varids: 
+    """
+    if isinstance(times, tuple):
+        intermediate_output = True
+    else:
+        intermediate_output = False
+
+    t0 = times[0]
+    if t0 == 0:
+        _times = times
+    else:
+        _times = [0] + list(times)
+
+    if p is not None:
+        net.p = p
+
+    if x0 is None:
+        x0 = net.x0
+
+    if rtol is None:
+        rtol = RTOL
+
+    if not hasattr(net, 'res_function'):
+        net.compile()
+
+    out = Dynamics.integrate_sensitivity(net, _times, rtol=[rtol]*net.xdim, 
+                                         fill_traj=intermediate_output)
+
+    if varids is not None:
+        if set(varids) < set(net.varids):
+            varids = itertools.product(varids, net.pids)
+        out = out.copy_subset(varids)
+
+    traj = out.values
+    times = out.timepoints
+    varids = out.key_column.keys()
+
+    if t0 != 0:
+        idx_t0 = list(times).index(t0)
+        times = times[idx_t0:]
+        traj = traj[idx_t0:]
+
+    traj = Trajectory(traj, index=pd.Index(times, name='time'), columns=varids)
+
+    return traj
+
+
+
 def get_predict(net, expts, **kwargs):
+    """
+    """
+    # other cases have not been implemented yet
+    assert expts.nrow == 1 and expts.get_conditions() == [None]
 
-    def get_xt(net, times, varids, p=None, tol=None, to_DF=False, use_daeint=False):
-        if tol is None:
-            tol = TOL
-        
-        if times[0] != 0:
-            times = [0] + list(times)
-            prepend_zero = True
-        else:
-            prepend_zero = False
-            
-        if p is not None:
-            net.update_optimizable_vars(p)
-       
-        x0 = net.x0.copy()  # integration changes x0, hence the copying
-        
-        if not hasattr(net, 'res_function'):
-            net.compile()
-            
-        if use_daeint:
-            assert varids == net.xids
-            out = daskr.daeint(res=net.res_function, t=times, y0=x0, yp0=[0]*net.xdim, 
-                               atol=[tol]*net.xdim, rtol=[tol]*net.xdim, 
-                               intermediate_output=False, rpar=net.constantVarValues)
-            xt = out[0]
-            xt[0] = net.x0  # somehow daskr.daeint messes up the first timepoint
-        else:
-            # Switch to Dynamics.integrate because of presumably easier indexing 
-            # of requested varids.
-            # Be careful of Dynamics.integrate's handling of times, esp. when 
-            # the initial time is not zero.    
-            traj = Dynamics.integrate(net, times, params=p, fill_traj=False,
-                                      rtol=[tol]*net.xdim, atol=[tol]*net.xdim)
-            xt = traj.copy_subset(varids).values
-            
-        if prepend_zero:
-            xt = xt[1:]
-            times = times[1:]
-        
-        if to_DF:
-            return util.DF(xt, index=times, columns=varids)
-        else:
-            return xt
+    varids = expts.get_varids()
+    times = expts.get_times()
 
-
-    def get_dxtdp(net, times, varids, p=None, tol=None, to_DF=False):
-        if tol is None:
-            tol = TOL
-
-        if times[0] != 0:
-            times = [0] + list(times)
-            prepend_zero = True
-        else:
-            prepend_zero = False
-            
-        straj0 = Dynamics.integrate_sensitivity(net, times, params=p, rtol=tol)
-        straj = straj0.copy_subset(itertools.product(varids, net.pids))
-
-        if prepend_zero:
-            dat = np.vstack(np.hsplit(straj.values[1:], len(varids)))
-            times = times[1:]
-        else:
-            dat = np.vstack(np.hsplit(straj.values, len(varids)))
-            
-        if to_DF:
-            return util.DF(dat, index=itertools.product(varids, times), 
-                            columns=net.pids)
-        else:
-            return dat
-            
-
-    assert expts.conds == [()], 'condition is not just wildtype.'
-    #assert util.flatten(expts['varids']) == net.xids  # the restriction can be relaxed later
-    varids = util.flatten(expts['varids'])
+    atol = kwargs.pop('atol', None)
+    rtol = kwargs.pop('rtol', None)
     
+    def _f(p):
+        traj = integrate(net, list(times), varids=varids, p=p, atol=atol, 
+                         rtol=rtol)
+        return traj.T.values.flatten()
     
-    def f(p):
-        net.update(t=0)
-        return get_xt(net, times util.flatten(expts['times']), p=p, varids=varids, **kwargs).T.flatten()
-        
-    
-    def Df(p):
-        net.update(t=0)
-        return get_dxtdp(net, times util.flatten(expts['times']), p=p, varids=varids, **kwargs)
+    def _Df(p):  # note that sensitivity integration also returns x(t)
+        straj = integrate_sensitivity(net, list(times), varids=varids, p=p, 
+                                      rtol=rtol)
+        return np.vstack([straj.values[:,i*net.pdim:(i+1)*net.pdim]
+                          for i in range(len(varids))])
 
-    pred = predict.Predict(f=f, Df=Df, p0=net.p0, pids=net.pids, 
-                           yids=expts.yids, expts=expts)
+    pred = predict.Predict(f=_f, Df=_Df, p0=net.p0, pids=net.pids, 
+                           yids=expts.get_yids(), expts=expts)
     
     return pred
-"""
